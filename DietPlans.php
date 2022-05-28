74beae02a66d1d9e4374823a20b3cc8980fd44c6
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-05-28 12:43:17 +0530

    Add changes

--- DietPlans.php
+++ DietPlans.php
@@ -1,55 +1,65 @@
 <html>
+
 <head>
-<link rel="stylesheet" href="CSS/home-style.css">
-  <link rel="stylesheet" href="./CSS/consult.css">
+    <link rel="stylesheet" href="CSS/home-style.css">
+    <link rel="stylesheet" href="./CSS/consult.css">
+    <style>
+        html {
+            background: url(./assets/img5.jpg);
+
+            /* background-size: cover; */
+        }
+    </style>
 </head>
 <header id="header">
-  <div id="brand">
-     <img id="header-img" src="assets/icon.png" href="home.php"/>
-  </div>
-  <nav id="nav-bar">
-    <a class="nav-link" href="home.php">Home</a>
-    <a class="nav-link" href="FindGroup.php">Events</a>
-    <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
-    <a class="nav-link" href="Blog.php">Blog</a>
-    <a class="nav-link" href="profile.php">Profile</a>
-  </nav>
+    <div id="brand">
+        <img id="header-img" src="assets/icon.png" href="home.php" />
+    </div>
+    <nav id="nav-bar">
+        <a class="nav-link" href="home.php">Home</a>
+        <a class="nav-link" href="FindGroup.php">Events</a>
+        <a class="nav-link" href="ConsultWithDoctor.php">Consult</a>
+        <a class="nav-link" href="Blog.php">Blog</a>
+        <a class="nav-link" href="profile.php">Profile</a>
+    </nav>
 </header>
+
 <body style="margin-top: 150px;">
     <div id="home-content">
-    <h1 style="font-size:50px; color:">Diet Plans</h1>
+        <h1 style="font-size:50px;">Diet Plans</h1>
     </div>
     <div id="content-home">
-            <div id='doc-results'>
-                <div class='product-card'>
+        <div id='doc-results'>
+            <div class='product-card'>
                 <img src='assets/diet-plan.jpg' class='product-img'></img>
-                     <div class='product-content'>
-                        <h1 class='product-name'>Ketogenic Diet</h1>
-                        <h3 style="color:white;">Low carb diet for weight loss</h3>
-                        <button onclick="document.location='https://calendar.google.com/calendar/u/0?cid=MnUxbzdxaWE3cDRzNXZmOWJzb3VmNHFpZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'"  class="doc-button">Add to my calender</Button>
-                    </div></a>
-                </div>
+                <div class='product-content'>
+                    <h1 class='product-name'>Ketogenic Diet</h1>
+                    <h3 style="color:white;">Low carb diet for weight loss</h3>
+                    <button style="background-color: #4285F4;cursor:pointer" onclick="document.location='https://calendar.google.com/calendar/u/0?cid=MnUxbzdxaWE3cDRzNXZmOWJzb3VmNHFpZWtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
             </div>
-            <div id='doc-results'>
-                <div class='product-card'>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card'>
                 <img src='assets/diet-plan.jpg' class='product-img'></img>
-                     <div class='product-content'>
-                        <h1 class='product-name'>Indian Diet plan for weight loss</h1>
-                        <h3 style="color:white;">Indian meal plans with low carb intake</h3>
-                        <button onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'"class=iet-plan-button">Add to my calender</Button>
-                    </div></a>
-                </div>
+                <div class='product-content'>
+                    <h1 class='product-name'>Indian Diet plan for weight loss</h1>
+                    <h3 style="color:white;">Indian meal plans with low carb intake</h3>
+                    <button style="background-color: #4285F4;cursor:pointer" onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
             </div>
-            <div id='doc-results'>
-                <div class='product-card'>
+        </div>
+        <div id='doc-results'>
+            <div class='product-card'>
                 <img src='assets/diet-plan.jpg' class='product-img'></img>
-                     <div class='product-content'>
-                        <h1 class='product-name'>High protein meal plan</h1>
-                        <h3 style="color:white;">Protein-rich meals for muscle training</h3>
-                        <button onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
-                    </div></a>
-                </div>
+                <div class='product-content'>
+                    <h1 class='product-name'>High protein meal plan</h1>
+                    <h3 style="color:white;">Protein-rich meals for muscle training</h3>
+                    <button style="background-color: #4285F4;cursor:pointer" onclick="document.location='https://calendar.google.com/calendar/u/0?cid=aGxndHJsYTdoZGliZGc5ZmJvZmwwbTRiNDRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'" class="doc-button">Add to my calender</Button>
+                </div></a>
             </div>
+        </div>
     </div>
 </body>
+
 </html>
