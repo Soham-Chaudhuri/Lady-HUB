4745950d9da23336fa83cb6f650acc5fd3ee3328
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-05-28 11:56:06 +0530

    Consult Done

--- ConsultWithDoctor.php
+++ ConsultWithDoctor.php
@@ -1,8 +1,13 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="./CSS/consult.css">
+    <!-- <link rel="stylesheet" href="./CSS/consult.css"> -->
     <link rel="stylesheet" href="./CSS/home-style.css">
+    <style>
+        html {
+            background: url("./assets/img2.jpg");
+        }
+    </style>
 </head>
 <header id="header">
     <div id="brand">
@@ -18,37 +23,37 @@
 </header>
 
 <body style="margin-top: 200px;">
-    <div id="home-content">
-        <h1 style="font-size:50px; color:#ff3256">Connect with Doctors and Counselor</h1>
+    <div id="home-content" style="border: 5px solid black;color:#2d2d2d;">
+        <h1 style="font-size:50px; color:darkblue">Connect with Doctors and Counselor</h1>
     </div>
-    <div id="content-home">
+    <div id="content-home" style="display: flex; flex-direction:column; align-items:center;justify-content:center">
         <div id='doc-results'>
-            <div class='product-card' style="width:90%; display: flex;">
+            <div class='product-card' style="width: 40%;background-color:aqua">
                 <img src='assets/doc.jpg' class='product-img'></img>
                 <div class='product-content'>
                     <h1 class='product-name'>Dr Shimali Patle</h1>
-                    <h3 style="color:red;">Gynaecologist(M.S.) MBBS</h3>
-                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin-bottom:2rem;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                    <h3 style="color:#ff3256;">Gynaecologist(M.S.) MBBS</h3>
+                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
                 </div></a>
             </div>
         </div>
         <div id='doc-results'>
-            <div class='product-card' style="width:90%;    justify-content: flex-start;">
+            <div class='product-card' style="width:40%;background-color:aqua">
                 <img src='assets/doc.jpg' class='product-img'></img>
                 <div class='product-content'>
                     <h1 class='product-name'>Sakshi Bhandarkar</h1>
-                    <h3 style="color:red;">Counselor and Therapist</h3>
-                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin-bottom:2rem;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                    <h3 style="color:#ff3256;">Counselor and Therapist</h3>
+                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
                 </div></a>
             </div>
         </div>
         <div id='doc-results'>
-            <div class='product-card' style="width:90%;    align-items: center;">
+            <div class='product-card' style="width:40%;background-color:aqua">
                 <img src='assets/doc.jpg' class='product-img'></img>
                 <div class='product-content'>
                     <h1 class='product-name'>Dr Anjali Patle</h1>
-                    <h3 style="color:red;">Pyschology(M.S.) MBBS</h3>
-                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin-bottom:2rem;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
+                    <h3 style="color:#ff3256;">Pyschology(M.S.) MBBS</h3>
+                    <a href="./video_call/index.php"><button class="doc-button" style="padding: 0.6rem;margin:2rem 0 2rem 0;border-radius:5rem;background-color:#ff3256;cursor:pointer;border:5px solid #0dd;color:aliceblue;font-size:1rem;font-weight:600">Connect</Button></a>
                 </div></a>
             </div>
         </div>
